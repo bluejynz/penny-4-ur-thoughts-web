7a5e889966864d5739d962dcf67e8057f10d7438
Dany Falk <dany_falk@hotmail.com>
2025-01-21 15:22:14 -0300

    finalizing deploy configs and adding temp icon

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: '/penny-4-ur-thoughts-web/',
     plugins: [react()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
+    },
+    build: {
+        outDir: "build",
     },
 });
